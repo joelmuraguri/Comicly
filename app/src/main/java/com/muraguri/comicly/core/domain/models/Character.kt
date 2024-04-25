3590447cb093d75a7c61509fe6ec1ff66b32867c
joelmuraguri <joelkmuraguri21@gmail.com>
2024-04-25 16:17:36 +0300

    initial commit

---
+++ app/src/main/java/com/muraguri/comicly/core/domain/models/Character.kt
@@ -0,0 +1,9 @@
+package com.muraguri.comicly.core.domain.models
+
+data class Character(
+    val id : Int,
+    val name : String,
+    val alias : String,
+    val image : String,
+    val siteLink : String
+)
---
+++ app/src/main/java/com/muraguri/comicly/core/domain/models/Volumes.kt
@@ -0,0 +1,8 @@
+package com.muraguri.comicly.core.domain.models
+
+data class Volumes(
+    val id : Int,
+    val title : String,
+    val image : String,
+    val siteLink : String
+)
