9a2c68b756ad9921e35fd586528a85202d7f6dd0
joelmuraguri <joelkmuraguri21@gmail.com>
2024-04-27 22:32:02 +0300

    fix padding

--- app/src/main/java/com/muraguri/comicly/ui/connectivity/components/NetworkUnavailable.kt
+++ app/src/main/java/com/muraguri/comicly/ui/connectivity/components/NetworkUnavailable.kt
@@ -1,18 +1,15 @@
 package com.muraguri.comicly.ui.connectivity.components
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -47,7 +44,7 @@ fun NetworkUnavailable(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
-                .padding(8.dp)
+                .padding( horizontal = 8.dp, vertical = 15.dp)
         ) {
             Icon(
                 painter = painterResource(id = R.drawable.round_wifi_off_24),
